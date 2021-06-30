af2fdd76cfe2d075234b1ff6907e98766dae8b57
Rowan O'Connor <oconnorrowand@gmail.com>
2021-06-29 22:00:44 -0400

    Add logic for like button

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -13,6 +13,7 @@ import moment from 'moment';
 // Material UI Components
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
@@ -23,6 +24,24 @@ const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  console.log(post);
+
+  const LikeIcon = () => {
+    if (post.likes.length > 0) {
+      return (post.likes.find((like) => like === (user?.googleId || user?._id)) === -1)
+        ? (
+          <ThumbUpAltOutlined />
+        )
+        : (
+          <ThumbUpAltIcon />
+        );
+    }
+
+    return <ThumbUpAltOutlined />;
+  }
+
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
@@ -30,15 +49,17 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button
-          style={{color: 'white' }}
-          size="small"
-          onClick={() => setCurrentId(post._id)}
-        >
-          <MoreHorizIcon fontSize="default" />
-        </Button>
-      </div>
+      { user && 
+        <div className={classes.overlay2}>
+          <Button
+            style={{color: 'white' }}
+            size="small"
+            onClick={() => setCurrentId(post._id)}
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      }
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
@@ -48,11 +69,13 @@ const Post = ({ post, setCurrentId }) => {
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button color="primary" onClick={() => dispatch(likePost(post._id))}>
-          <ThumbUpAltIcon /> &nbsp; {post.likes.length}
+          <LikeIcon /> &nbsp; {post.likes.length}
         </Button>
-        <Button color="primary" onClick={() => dispatch(deletePost(post._id))}>
-          <DeleteIcon />
-        </Button>
+        { user && 
+          <Button color="primary" onClick={() => dispatch(deletePost(post._id))}>
+            <DeleteIcon />
+          </Button>
+        }
       </CardActions>
     </Card>
   );
